10e112241b01447a6a9da518fdc3cc3965504355
Eric Kever <lolbummer@gmail.com>
2012-03-22 16:35:43 -0400

    Initial commit.

---
+++ twitterfly.js
@@ -0,0 +1,17 @@
+(function($){
+	var defaults = {
+			feeds : [],
+			hide_linked_tweets : false,
+			hide_replies : true,
+			link_hashtags : true,
+			link_handles : true,
+			hide_retweets : false,
+			tweet_html : ""
+		},
+		placeholders : {
+			image : "{{IMAGE}}",
+			tweet : "{{TWEET}}",
+			time : "{{TIME}}",
+			
+		}
+})(jQuery);
