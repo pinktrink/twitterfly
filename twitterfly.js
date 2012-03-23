84d676f6a3b051312669862b3f499572bbb9e248
Eric Kever <lolbummer@gmail.com>
2012-03-22 20:19:41 -0400

    Safety commit.

--- twitterfly.js
+++ twitterfly.js
@@ -1,17 +1,45 @@
 (function($){
-	var defaults = {
-			feeds : [],
-			hide_linked_tweets : false,
-			hide_replies : true,
-			link_hashtags : true,
-			link_handles : true,
-			hide_retweets : false,
-			tweet_html : ""
-		},
-		placeholders : {
-			image : "{{IMAGE}}",
-			tweet : "{{TWEET}}",
-			time : "{{TIME}}",
+	$.fn.twitterfly = function(opts){
+		var feedDefaults = {
+				hide_linked_tweets : false,
+				hide_replies : true,
+				link_hashtags : true,
+				link_handles : true,
+				hide_retweets : false
+			},
+			defaults = {
+				tweet_html : "",
+				loading_html : "Loading tweets...",
+				max : 5
+			},
+			placeholders = {
+				image : "{{IMAGE}}",
+				tweet : "{{TWEET}}",
+				time : "{{TIME}}"
+			};
+
+		$.extend(opts, defaults);
+
+		if(opts.feeds.length === 0){
+			$.error("At least one feed is required for twitterfly.");
+
+			return;
+		}
+
+		if(opts.tweet_html.length === 0){
+			$.error("Tweet HTML is required for twitterfly.");
+		}
+
+		this.html(opts.loading_html);
+
+		for(var i = 0, j = opts.feeds.length; i < j; i++){
+			$.extend(opts.feeds[i], feedDefaults);
+
+			if(!opts.feeds[i].handle){
+				$.error("Feed " + i + " has no handle, skipping.");
+			}
+
 			
 		}
+	};
 })(jQuery);
